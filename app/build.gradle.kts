e3d87ad2d52ec689ca37cd833102044d5b3ac7ac
gorthol <dreszgz@gmail.com>
2024-09-09 12:32:41 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.gorthol.ktomg"
+        applicationId = "de.wagenknecht.backloggd"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
